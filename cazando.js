8c5337a19ec5bece732662b71c463fab60a46594
pauchi1 <p.chiluisa118@gmail.com>
2026-09-11 16:58:13 -0500

    funcion iniciarJuego y graficarComida agregados

--- cazando.js
+++ cazando.js
@@ -1,7 +1,31 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 40;
+const ANCHO_GATO = 30;
+const ALTO_COMIDA = 20;
+const ANCHO_COMIDA = 20;
+
+function iniciarJuego() {
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+    graficarComida();
+    graficarGato();
+}
+
 function graficarGato() {
     ctx.fillStyle ="#949494";
-    ctx.fillRect(canvas.width/2,canvas.height/2,20,20);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "#F09F69";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
